bd570bae20d69e81d3360f73dd863f31331298f0
bubbagump2020 <bubbagum2020@gmail.com>
2019-10-23 13:03:17 -0500

    Able to write and save reviews

--- src/components/ReviewForm.js
+++ src/components/ReviewForm.js
@@ -10,33 +10,53 @@ class ReviewForm extends React.Component{
         }
     }
 
+    createTitle = (e) => {
+        this.setState({
+            title: e.target.value
+        })
+    }
+
+    createContent = (e) => {
+        this.setState({
+            content: e.target.value
+        })
+    }
+
     createReview = (e) => {
         e.preventDefault()
-        console.log(e.target.value)
+        const token = localStorage.getItem('token')
         fetch('http://localhost:3001/reviews', {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type' : 'application/json',
+                'Authorization': `Bearer ${token}`
             },
             body: JSON.stringify({
+                user_id: this.props.userID,
                 title: this.state.title,
                 content: this.state.content
             })
         })
+        .then(response => response.json())
+        .then(review => {
+            console.log(review)
+        })
     }
 
     render(){
+        // console.log(this.state.title)
+        // console.log(this.state.content)
         return(
             <div>
                 <h2>This is the Form to add reviews</h2>
                 <form onSubmit={e => this.createReview(e)}>
                     <div>
                         <label>Title</label><br></br>
-                        <input type="text" value={this.state.title} onChange={e => this.setState({ title: e.target.value })}/>
+                        <input type="text" onChange={e => this.createTitle(e)}/>
                     </div>
                     <div>
                         <label>Write Your Review</label><br></br>
-                        <textarea rows="10" cols="50" value={this.state.content} onChange={e => this.setState({ content: e.target.value })} />
+                        <textarea rows="10" cols="50" onChange={e => this.createContent(e)}/>
                     </div>
                     <input type="submit" value="Submit Your Review!" />
                 </form>
