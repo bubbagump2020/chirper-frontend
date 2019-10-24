f4aa82dba10df9e04693b3a98f41764997adc307
bubbagump2020 <bubbagum2020@gmail.com>
2019-10-24 09:28:12 -0500

    Still working on the musicplayer

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -1,49 +1,27 @@
 import React from 'react'
 import SpotifyPlayer from 'react-spotify-player'
 
-var ClientOAuth2 = require('client-oauth2')
 
-var githubAuth = new ClientOAuth2({
-    clientId: 'abc',
-    clientSecret: '123',
-    accessTokenUri: 'https://github.com/login/oauth/access_token',
-    authorizationUri: 'https://github.com/login/oauth/authorize',
-    redirectUri: 'http://example.com/auth/github/callback',
-    scopes: ['notifications', 'gist']
-  })
+    
 
-class SongPlayer extends React.Component{
-    constructor(){
-        super()
-        this.state = {
-            token: null,
-            item: {
-              album: {
-                images: [{ url: "" }]
-              },
-              name: "",
-              artists: [{ name: "" }],
-              duration_ms:0,
-            },
-            is_playing: "Paused",
-            progress_ms: 0
-        }
-    }
 
-    componentDidMount(){
-        const token = localStorage.getItem('token')
-        githubAuth.jwt.getToken(token)
-        .then(function (user) {
-            console.log(user)
-        })
+class SongPlayer extends React.Component {
+
+
+    componentDidMount() {
+        fetch('http://localhost:3001/auth/spotify')
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+            })
     }
 
 
 
-    render(){
-        console.log(this.state.token)
-        return(
+    render() {
+        return (
             <div>
+                
             </div>
         )
     }
